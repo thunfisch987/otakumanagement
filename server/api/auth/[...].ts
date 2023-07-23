54d303e9eb2022244729e0fbd0cfd1f0f5d8a389
Oliver Sader <thunfisch987@gmail.com>
2023-07-23 11:58:57 +0200

    restrict signIn to only specific accounts

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,4 +1,5 @@
 import { NuxtAuthHandler } from '#auth';
+import { get } from '@vercel/edge-config';
 import GoogleProvider from 'next-auth/providers/google';
 
 export default NuxtAuthHandler({
@@ -13,4 +14,14 @@ export default NuxtAuthHandler({
 			clientSecret: process.env.CLIENT_SECRET!,
 		}),
 	],
+	callbacks: {
+		async signIn({ profile }) {
+			if (profile?.email) {
+				if (`${await get('authorizedmails')}`.includes(profile.email)) {
+					return Promise.resolve(true);
+				}
+			}
+			return Promise.resolve(false);
+		},
+	},
 });
