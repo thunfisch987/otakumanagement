c46ba445084b4f588fb12c3ba1a7679ab78df327
Oliver Sader <thunfisch987@gmail.com>
2023-08-03 11:12:40 +0200

    add login and logout sites

---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+	<UButton @click="signIn('google')">Login</UButton>
+</template>
+
+<script setup lang="ts">
+const { signIn } = useAuth();
+definePageMeta({
+	middleware: 'guest-only',
+});
+</script>
---
+++ pages/logout.vue
@@ -0,0 +1,10 @@
+<template>
+	<UButton @click="signOut()">Logout</UButton>
+</template>
+
+<script setup lang="ts">
+const { signOut } = useAuth();
+definePageMeta({
+	middleware: 'auth',
+});
+</script>
