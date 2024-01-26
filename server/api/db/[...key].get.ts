42e1cfbb74f1c85e151967a813a3dbc3afd059ae
thunfisch987 <thunfisch987@gmail.com>
2024-01-26 21:51:04 +0100

    :art::b:remove useless else (early return)

--- server/api/db/[...key].get.ts
+++ server/api/db/[...key].get.ts
@@ -3,7 +3,6 @@ import { get } from '@vercel/edge-config';
 export default defineEventHandler(async (event) => {
 	if (event.context.params?.key) {
 		return `${await get(event.context.params.key)}`;
-	} else {
-		return 'please provide key';
 	}
+	return 'please provide key';
 });
